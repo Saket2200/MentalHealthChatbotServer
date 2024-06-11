773e4ab09c774e21d3c60c13f803347f2ac729c9
Saket2200 <120158757+Saket2200@users.noreply.github.com>
2024-06-11 21:34:14 +0530

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# app.py
+
 from flask import Flask, request, jsonify
 import pandas as pd
 import nltk
@@ -44,4 +46,4 @@ def chat():
     return jsonify({"response": response})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8080)
